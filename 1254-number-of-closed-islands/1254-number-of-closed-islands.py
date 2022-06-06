d253793d42944dc6755947b6cb4353ae5f41692e
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-06-06 14:33:47 +0300

    Time: 209 ms (33.27%), Space: 14.4 MB (67.06%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -0,0 +1,20 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        ans = 0
+        def solve(x, y):
+            if x < 0 or y < 0 or x > len(grid) - 1 or y > len(grid[0]) - 1:
+                return False
+            if grid[x][y]:
+                return True 
+            grid[x][y] = 1
+            return all([solve(x + 1, y),
+                        solve(x - 1, y),
+                        solve(x, y + 1),
+                        solve(x, y - 1)])
+            
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if not grid[i][j]:
+                    ans+=solve(i, j)
+        return ans 
+        
