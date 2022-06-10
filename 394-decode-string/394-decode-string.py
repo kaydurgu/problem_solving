30a35ae7eec04d9c9f7f3c096d5bed58f2056465
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-06-10 18:04:48 +0300

    Time: 44 ms (42.99%), Space: 14 MB (19.66%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,12 +1,9 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         def solve(s):
-            letter = ''
-            digit = ''
-            multi = ''
-            cnt = 0
+            letter, digit, multi, cnt = '', '', '', 0
             for i in range(len(s)):
-                if cnt == 0:
+                if not cnt:
                     if s[i].isalpha():
                         letter+=s[i]
                     if s[i].isdigit():
@@ -14,13 +11,10 @@ class Solution:
                 if s[i] in ['[', ']']:
                     if s[i] == '[':
                         cnt+=1
-                        if cnt == 1:
-                            continue
                     else:
                         cnt-=1
-                    if cnt == 0:
-                        #print(letter, digit , multi,s[i + 1:])
-                        return letter + int(digit) * solve(multi) + solve(s[i + 1:])
+                    if not cnt:
+                        return letter + int(digit) * solve(multi[1:]) + solve(s[i + 1:])
                 if cnt > 0:
                     multi+=s[i]
             return letter
