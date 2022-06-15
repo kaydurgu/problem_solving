03ec20d9035c812c05a6edc334cdcd418649a6be
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-06-15 09:39:27 +0300

    Time: 318 ms (35.00%), Space: 14.4 MB (48.74%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,7 +1,13 @@
-from collections import defaultdict
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        dp = defaultdict(int)
-        for w in sorted(words,key=len):
-            dp[w] = max( dp[w[:i]+w[i+1:]] + 1 for i in range(len(w)))
-        return max(dp.values())
+        d = dict()
+        for word in words:
+            d[word] = 1
+        longest = 1
+        for word in sorted(words, key=len):
+            for i in range(len(word)):
+                prev = word[:i] + word[i + 1:]
+                if prev in d:
+                    d[word] = max(d[word], d[prev] + 1)
+            longest = max(longest, d[word])
+        return longest
