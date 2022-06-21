0485df33ebb29506fd7386bac3dbb6435b2c5e48
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-06-21 14:18:46 +0300

    Time: 1234 ms (13.10%), Space: 28.6 MB (16.41%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -5,26 +5,21 @@ class Solution:
         used_bricks = []
         for i in range(1, len(heights)):
             if cur > heights[i]:
-                #print (i, cur , heights[i] , 'cur > heights' , bricks, ladders)
                 ans += 1 
             else:
+                bisect.insort(used_bricks, heights[i] - cur)
                 if heights[i] - cur <= bricks:
                     bricks = bricks - (heights[i] - cur)
                     ans += 1
-                    bisect.insort(used_bricks, heights[i] - cur)
-                    #print (i, cur , heights[i] , 'heights[i] - cur <= bricks' , bricks, ladders)
                 else:
-                    bisect.insort(used_bricks, heights[i] - cur)
                     if ladders > 0:
-                        bricks+=used_bricks[-1]
+                        bricks += used_bricks[-1]
                         used_bricks.pop()
-                        bricks-=(heights[i] - cur)
-                        ladders-=1
-                        #print (i, cur , heights[i] , 'else' , bricks, ladders)
-                        ans +=1
+                        bricks -= (heights[i] - cur)
+                        ladders -= 1
+                        ans += 1
                     else:
                         break
             cur = heights[i]
-        print (len(heights))
         return ans 
             
