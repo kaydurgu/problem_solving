a72f69b6a626121cea79bca4b34898ebd90b151a
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-06-28 08:52:10 +0300

    Time: 3325 ms (5.00%), Space: 27.9 MB (5.13%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -2,24 +2,19 @@ class Solution:
     def minDeletions(self, s: str) -> int:
         mp = collections.defaultdict(int)
         cnter = collections.Counter(s)
-        lower = string.ascii_lowercase
-        cnter = dict(sorted(cnter.items(),key = lambda item: item[1]))
         nakaz = []
         for key, val in cnter.items():
             if mp[val]:
                 nakaz.append(val)
             else:
                 mp[val]+=1
-        arr = [0]
+        arr, cnt = [0], 0
         for idx in range(1, 10 ** 5):
             if not mp[idx]:
                 arr.append(idx)
-        cnt = 0
         for i in nakaz:
             x = arr[bisect.bisect_left(arr, i) - 1]
-            if x == 0:
-                cnt += i
-            else:
+            cnt += (i - x)
+            if x != 0:
                 arr.remove(x)
-                cnt += (i - x)
         return cnt
