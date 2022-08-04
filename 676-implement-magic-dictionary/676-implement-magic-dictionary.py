793cb9ab47fab4654024e935b3c48c95920646dd
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-08-04 17:27:57 +0300

    Time: 424 ms (41.41%), Space: 14.1 MB (97.47%) - LeetHub

---
+++ 676-implement-magic-dictionary/676-implement-magic-dictionary.py
@@ -0,0 +1,10 @@
+class MagicDictionary(object):
+    def __init__(self):
+        self.buckets = collections.defaultdict(list)
+
+    def buildDict(self, words):
+        for word in words:
+            self.buckets[len(word)].append(word)
+
+    def search(self, word):
+        return any(sum(a!=b for a,b in zip(word, candidate)) == 1 for candidate in self.buckets[len(word)])
